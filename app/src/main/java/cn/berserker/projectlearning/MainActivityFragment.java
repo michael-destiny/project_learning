221751418bd262b76e18ab4f71c315082f75d781
dsx <1553267648@qq.com>
2016-08-11 21:43:42 +0800

    1.upload code.

--- app/src/main/java/cn/berserker/projectlearning/MainActivityFragment.java
+++ app/src/main/java/cn/berserker/projectlearning/MainActivityFragment.java
@@ -17,6 +17,9 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // test update code.
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
